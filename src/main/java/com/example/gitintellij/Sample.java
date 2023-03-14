ba01ce9e92fbc2a3c792cfd5baedb6a3b64b9f15
Adrian Cosmin Nedelcu <91340261+AdrianCosmin18@users.noreply.github.com>
2023-03-14 23:56:07 +0200

    commit 1

--- src/main/java/com/example/gitintellij/Sample.java
+++ src/main/java/com/example/gitintellij/Sample.java
@@ -2,6 +2,6 @@ package com.example.gitintellij;
 
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hi, how are you all ?");
     }
 }
