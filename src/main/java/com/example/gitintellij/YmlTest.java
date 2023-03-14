04f23627b187e1b52b5305905b37389419a2b183
Adrian-Cosmin Nedelcu <anedelcu@cbn.ro>
2023-03-15 00:19:16 +0200

    commit on main branch

--- src/main/java/com/example/gitintellij/YmlTest.java
+++ src/main/java/com/example/gitintellij/YmlTest.java
@@ -2,6 +2,6 @@ package com.example.gitintellij;
 
 public class YmlTest {
     public static void main(String[] args) {
-        System.out.println("hello, from dev branch");
+        System.out.println("hello, from main");
     }
 }
