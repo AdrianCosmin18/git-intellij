69be48f4cf0d69c8d39e2cf35ce00303e83b1cb6
Adrian-Cosmin Nedelcu <anedelcu@cbn.ro>
2023-03-15 00:28:32 +0200

    commit on feature branch

--- src/main/java/com/example/gitintellij/Sample.java
+++ src/main/java/com/example/gitintellij/Sample.java
@@ -3,6 +3,6 @@ package com.example.gitintellij;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("hi all");
-        System.out.println("hi, how are you all ?");
+        System.out.println("hi, how are you ? ");
     }
 }
